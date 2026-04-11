931b1c80fa8f82e664aa330dc086646d9ca24d00
Clément Auvray <114954624+Erismos@users.noreply.github.com>
2026-04-11 16:13:43 -0400

    requirements

--- pipeline-suivi-P4/install.sh
+++
@@ -1,4 +0,0 @@
-pip install ultralytics          # YOLOv11 + ByteTrack intégré
-pip install motmetrics           # calcul MOTA, MOTP, ID switches
-pip install opencv-python        # traitement vidéo
-pip install pandas numpy         # manipulation des données
